23b1d94f274e6d0fc42bf2a682e52bfd378890a9
Monika <monikad0602@gmail.com>
2022-04-30 12:37:12 +0200

    implement random dice rolling

--- game/Dice.py
+++ game/Dice.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from pygame import Vector2
 
@@ -5,16 +7,15 @@ from pygame import Vector2
 class Dice:
     def __init__(self):
         self.cellSize = Vector2(40, 40)
-        self.dice1Texture, self.dice2Texture = self.pick_dice()
+        self.dice1Texture, self.dice2Texture = self.load_dice()
 
         self.dice1Pos = Vector2(973, 569)
         self.dice2Pos = Vector2(1039, 569)
 
     @staticmethod
-    def pick_dice():
-        dice1_number = "1"
-        dice2_number = "3"
-        seq1 = ("./img/dice_", dice1_number, ".png")
-        seq2 = ("./img/dice_", dice2_number, ".png")
+    def load_dice():
+        dice1_number, dice2_number = random.randint(1, 6), random.randint(1, 6)
+        seq1 = ("./img/dice_", str(dice1_number), ".png")
+        seq2 = ("./img/dice_", str(dice2_number), ".png")
 
         return pygame.image.load("".join(seq1)).convert(), pygame.image.load("".join(seq2)).convert()
